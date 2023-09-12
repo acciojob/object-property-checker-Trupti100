54ae962bdab4c01ef3de554dd2b1e93cde717082
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-12 12:43:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
 function hasKey(key) {
-  //   write your code here
+  // Check if the key exists in the sampleObject
+  if (sampleObject.hasOwnProperty(key)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// Define the global sampleObject
+const sampleObject = {
+  red: "#FF0000",
+  green: "#00FF00",
+  white: "#FFFFFF"
+};
+
+// Example usages:
+console.log(hasKey("red"));    // Output: true
+console.log(hasKey("blue"));   // Output: false
+console.log(hasKey("white"));  // Output: true
